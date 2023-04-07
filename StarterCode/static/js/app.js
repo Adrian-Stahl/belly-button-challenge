b1a0503ab84b6e8af614bd96e60f42dbfd2c2a7d
Adrian <carlos.adrian.stahl@gmail.com>
2023-04-06 20:13:11 -0400

    commit from VBT

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,96 +1,162 @@
-// Use sample_values as the values for the bar chart.
-// Use otu_ids as the labels for the bar chart.
-// Use otu_labels as the hovertext for the chart.
-
-
-function getPlots(id) {
-    d3.json("samples.json").then (sampledata=>{
-        console.log(sampledata)
-        var id = sampledata.samples[0].otu_ids;
-        console.log(id)
-        var theSampleValues = sampledata.samples[0].sample_values.slice(0,10).reverse();
-        console.log(theSampleValues)
-        var theLabels = sampledata.samples[0].otu_labels.slice(0,10);
-        console.log(theLabels)
-//2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-        var otu_top10 =  (sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
-        var otu_id = otu_top10.map(d => "OTU" +d);
-        console.log(`OTU IDS: ${otu_id}`)
-        var theLabels = sampledata.samples[0].otu_labels.slice(0,10);
-        console.log(`otu_labels: ${theLabels}`)
-        var tracing = {
-            x:theSampleValues,
-            y:otu_id,
-            text: theLabels,
-            marker: {
-            color: 'green'},
-            type:"bar",
-            orientation: "h",
-        };
-        var data = [tracing];
-        var plotLayout = {
-            title: "Top 10 OTU",
-            yaxis:{ tickmode: "linear",},
-        
-            margin: {
-                l:100,
-                r:100,
-                t:100,
-                b:30
-            }
-        };
-    Plotly.newPlot("bar", data, plotLayout);
-        var tracingalfa = {
-            x:sampledata.samples[0].otu_ids,
-            y:sampledata.samples[0].sample_values,
-            mode: "markers",
-            marker: {
-                size: sampledata.samples[0].sample_values,
-                color: sampledata.samples[0].otu_ids
-            },
-            text: sampledata.samples[0].otu_labels
-    };
-// 3. Create a bubble chart that displays each sample.
-        var bubble_layout = {
-            xaxis:{title: "OTU ID"},
-            height:600,
-            width:1000
-        };
-        var data_alfa = [tracingalfa];
-
-    Plotly.newPlot("bubble", data_alfa, bubble_layout);
-    });
-}
-function getDemoInfo(id) {
-    d3.json("samples.json").then((data)=>{
-        var meta = data.meta;
-        console.log(meta)
-        var filtro = meta.filter(metadata => metadata.id.toString() === id)[0];
-        var demoInfo =d3.select("#sample-metadata");
-        demoInfo.html("");
-
-        Object.entries(filtro).forEach((key) => {
-            demoInfo.append("h5").text(key[0].toUpperCase() + ":" +key[1] + "\n");
-        });
-    });
-}
-function optionChanged(id) {
-    getPlots(id);
-    getDemoInfo(id);
-}
-
+// Display the default plot
 function init() {
-    var dd = d3.select("#selDataset");
+    d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json").then(function (data) {
+      console.log(data);
+        // Create array to hold all names (all ID names)
+        var names = data.samples.map(x=>x.id)
+        // Append an option in the dropdown
+        names.forEach(function(name) {
+            d3.select('#selDataset')
+                .append('option')
+                .text(name)
+            });
+  
+    // Create arrays for sample_values, OTU ids, and OTU labels        
+    var sample_values = data.samples.map(x=> x.sample_values);
+    var otu_ids = data.samples.map(x=> x.otu_ids);
+    var otu_label = data.samples.map(x=> x.otu_labels);
+    
+    // Get the top 10 OTU
+    var sorted_test = sample_values.sort(function(a, b){return b-a});
+    var top_ten = sorted_test.map(x => x.slice(0,10));
+    var sorted_ids = otu_ids.sort(function(a, b){return b-a});
+    var top_ids = sorted_ids.map(x =>x.slice(0,10));
+    var sorted_labels = otu_label.sort(function(a, b){return b-a});
+    var top_labels = sorted_labels.map(x =>x.slice(0,10));
+  
+    // Get the first ID to display on page on load
+    var firstID = data.metadata[0]// first id
+    var sampleMetadata1 = d3.select("#sample-metadata").selectAll('h1')
+    
+    //-------------------------------------------------
+    // Display the first ID's demographic information
+    var sampleMetadata = sampleMetadata1.data(d3.entries(firstID))
+    sampleMetadata.enter()
+                  .append('h1')
+                  .merge(sampleMetadata)
+                  .text(d => `${d.key} : ${d.value}`)
+                  .style('font-size','50%')
+  
+    sampleMetadata.exit().remove()
+  
+    // Create Bar Chart
+    // Create trace for bar chart
+    var trace1 = {
+        x : top_ten[0],
+        y : top_ids[0].map(x => "OTU" + x),
+        text : top_labels[0],
+        type : 'bar',
+        orientation : 'h',
+        transforms: [{
+            type: 'sort',
+            target: 'y',
+            order: 'descending',
+          }],
+        marker: {
+          color: 'rgb(27, 161, 187)',
+          opacity: 0.6,
+          line: {
+            color: 'rgb(8,48,107)',
+            width: 1.5
+          }
+        }
+    };
+    // Create layout
+    var layout1 = {
+        title : '<b>Top 10 OTU</b>',
+    };
+  
+    // Draw the bar chart
+    var data = [trace1];
+    var config = {responsive:true}
+    Plotly.newPlot('bar', data, layout1,config);
+  
+  
+    // Create a bubble chart 
+    // Create the trace for the bubble chart
+    var trace2 = {
+        x : otu_ids[0],
+        y : sample_values[0],
+        text : otu_label[0],
+        mode : 'markers',
+        marker : {
+            color : otu_ids[0],
+            size : sample_values[0]
+        }
+    };
+  
+    // Create layout
+    var layout2 = {
+        title: '<b>Bubble Chart</b>',
+        automargin: true,
+        autosize: true,
+        showlegend: false,
+            margin: {
+                l: 150,
+                r: 50,
+                b: 50,
+                t: 50,
+                pad: 4      
+    }};
+  
+    // Draw the bubble chart
+    var data2 = [trace2];
+    var config = {responsive:true}
+    Plotly.newPlot('bubble',data2,layout2,config);
+  
+    
 
-    d3.json("samples.json").then((data) => {
-        console.log(data)
-
-        data.names.forEach(function(name) {
-            dd.append("option").text(name).property("value");
-        });
-        getPlots(data.names[0]);
-        getDemoInfo(data.names[0]);
+  
+        // Create the layout
+        var layout1 = {
+            title: "<b>Top 10 OTU</b>"
+        };
+        var data1 = [trace];
+        var config = {responsive:true}
+  
+        // Plot the bar chart
+        Plotly.newPlot('bar', data1,layout1,config);
+  
+  
+        // Create a bubble chart 
+        // Create the trace for the bubble chart
+        var trace2 = {
+            x : test.map(x=> x.otu_ids)[0],
+            y : test.map(x => x.sample_values)[0],
+            text : test.map(x=> x.otu_labels),
+            mode : 'markers',
+            marker : {
+                color : test.map(x=> x.otu_ids)[0],
+                size : test.map(x => x.sample_values)[0]
+            }   
+        };
+  
+        // Create the layout
+        var layout2 = {
+            title: '<b>Bubble Chart</b>',
+            automargin: true,
+            autosize: true,
+            showlegend: false,
+            margin: {
+                l: 150,
+                r: 50,
+                b: 50,
+                t: 50,
+                pad: 4
+              }
+        };
+  
+        // Create the bubble chart
+        var data2 = [trace2];
+        var config = {responsive:true}
+        Plotly.newPlot('bubble', data2,layout2,config)
     });
-}
-
-init();
+  };
+  
+  
+  // Call updatePlotly
+  function optionChanged(id) {
+    updatePlotly(id);
+  };
+  
+  init();
